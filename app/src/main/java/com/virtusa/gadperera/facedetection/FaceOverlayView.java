99308b043cd12f03901c91f3494092a08d420ffd
Gayan Denaindra Perera <gadperera@cmdgadperera.local>
2017-02-22 17:52:10 +0530

    update project

---
+++ app/src/main/java/com/virtusa/gadperera/facedetection/FaceOverlayView.java
@@ -0,0 +1,27 @@
+package com.virtusa.gadperera.facedetection;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by gadperera on 2/22/17.
+ */
+
+public class FaceOverlayView extends View {
+    public FaceOverlayView(Context context) {
+        super(context);
+    }
+
+    public FaceOverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FaceOverlayView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public FaceOverlayView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
